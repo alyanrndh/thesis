bad64ba24e02adfc750b98ef6c4c0a5119128ac2
alya <56344623+alyanrndh@users.noreply.github.com>
2022-08-16 23:52:27 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,100 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import pickle
+
+from PIL import Image
+from sklearn import datasets
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+img = Image.open('iuli_logo.png')
+st.set_page_config(page_title = "Thesis Sample", page_icon = img)
+
+st.title('Comparison of Algorithm Performance in Heart Disease Classification')
+st.markdown("""
+            This app is the result of the implementation of the **Bachelor of Computer Science and Engineering Thesis**
+            by **Alya Nurindah Setiawan (11201812006)** which aims to find out the comparison of the results 
+            of the accuracy of 3 classification algorithms **(Naive Bayes, Classification and Regression Tree, and Random Forest)**
+            with the heart disease dataset. You can upload a dataset of heart disease patients to find out which algorithm is the most accurate.
+            """)
+
+heart_data = pd.read_csv('heartdisease1.csv')
+heartdisease = heart_data.drop(columns = ['target'])
+df = pd.concat([heartdisease], axis = 0)
+
+st.sidebar.header('Dataset')
+
+file = st.sidebar.file_uploader("Upload your file.", type = ["csv"])
+
+X = heart_data.drop(columns = ['target'])
+y = heart_data['target']
+
+st.subheader('Preprocessed Dataset')
+if file is not None:
+    st.write(df)
+else:
+    st.write('This is an example dataset, please upload your dataset file.')
+    st.write(df)
+
+st.subheader('Exploratory Data Analysis')
+st.write('Shape of the data is ', X.shape)
+
+st.sidebar.header('Model Selection')
+algorithm = st.sidebar.selectbox('Select the algorithm model.', ('Naive Bayes', 
+                                                          'Classification and Regression Tree',
+                                                          'Random Forest'))
+    
+def newparam(choose_algorithm):
+    params = dict()
+    if choose_algorithm == 'Classification and Regression Tree':
+        criterionCART = st.sidebar.selectbox('criterion', ('gini', 'entropy', 'log_loss'))
+        params['criterionCART'] = criterionCART
+        max_depthCART = st.sidebar.slider('max_depth', 2, 15)
+        params['max_depthCART'] = max_depthCART
+        
+    elif choose_algorithm == 'Random Forest':
+        n_estimators = st.sidebar.slider('n_estimators', 1, 100)
+        params['n_estimators'] = n_estimators
+        max_depthRF = st.sidebar.slider('max_depth', 2, 15)
+        params['max_depthRF'] = max_depthRF
+        criterionRF = st.sidebar.selectbox('criterion', ('gini', 'entropy', 'log_loss'))
+        params['criterionRF'] = criterionRF
+        
+    return params
+
+params = newparam(algorithm)
+            
+def classification(choose_algorithm, params):
+    algo = None
+    if choose_algorithm == 'Naive Bayes':
+        algo = GaussianNB()
+    elif choose_algorithm == 'Classification and Regression Tree':
+        algo = DecisionTreeClassifier(criterion = params['criterionCART'],
+                                      max_depth = params['max_depthCART'],
+                                      random_state = 0)
+    else:
+        algo = RandomForestClassifier(n_estimators = params['n_estimators'],
+                                      max_depth = params['max_depthRF'],
+                                      criterion = params['criterionRF'],
+                                      random_state = 0)
+    return algo
+
+algo = classification(algorithm, params)
+
+score = cross_val_score(algo, X, y, cv = 10)
+
+st.subheader('Results')
+st.write(f'Algorithm: {algorithm}')
+st.write('Accuracy: ', round(score.mean()*100,2), '%')
+st.write('Standard Deviation: ', round(score.std()*100,2), '%')
